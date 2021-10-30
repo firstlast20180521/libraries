91305bd7c6f7d48335d6e6f1cfecd9af90d5af1d
firstlast20180521 <39790459+firstlast20180521@users.noreply.github.com>
2021-10-30 21:25:40 +0900

    本当のリリース

--- Libraries/utilities/src/main/java/firstlast20180521/utilities/Debug.java
+++ Libraries/utilities/src/main/java/firstlast20180521/utilities/Debug.java
@@ -8,18 +8,21 @@ public class Debug {
 
     //所謂オーバーロードというもの
     public static void print() {
-        print("");
+        print("-");
     }
 
     public static void print(String text) {
-        int length = Thread.currentThread().getStackTrace().length;
+
+        StackTraceElement[] trace = Thread.currentThread().getStackTrace();
+
+        int length = trace.length;
 
         //System.out.println("start-------------------------------------------start");
         //for (int i = 0; i < length; i++) {
         //    System.out.println(String.format(Locale.JAPAN, "j===>%d, className===>%s, Method===>%s"
         //            , i
-        //            , Thread.currentThread().getStackTrace()[i].getClassName()
-        //            , Thread.currentThread().getStackTrace()[i].getMethodName()
+        //            , trace[i].getClassName()
+        //            , trace[i].getMethodName()
         //            )
         //    );
         //
@@ -28,15 +31,16 @@ public class Debug {
 
         String className = "";
         String methodName = "";
+        int lineNumber = 0;
 
         //for (int j = length - 1; j > -1; j--) {
-        //    String tempClassName = Thread.currentThread().getStackTrace()[j].getClassName();
+        //    String tempClassName = trace[j].getClassName();
         //    String[] x = tempClassName.split("\\.");
         //
         //    if (x.length > 0) {
-        //        if (x[0].equals("firstlast20180521")) {
+        //        if (x[0].equals("xxxxxxxxxxxx")) {
         //            className = tempClassName;
-        //            methodName = Thread.currentThread().getStackTrace()[j].getMethodName();
+        //            methodName = trace[j].getMethodName();
         //            break;
         //        }
         //    }
@@ -44,16 +48,21 @@ public class Debug {
         //}
 
         for (int j = length - 1; j > -1; j--) {
-            String tempClassName = Thread.currentThread().getStackTrace()[j].getClassName();
+            String tempClassName = trace[j].getClassName();
 
             if (tempClassName.equals(SELF_CLASS_NAME)) {
-                className = Thread.currentThread().getStackTrace()[j + 1].getClassName();
-                methodName = Thread.currentThread().getStackTrace()[j + 1].getMethodName();
+                //className = trace[j + 1].getClassName();
+
+                String name = trace[j + 1].getClassName();
+                String[] arr = name.split("\\.");
+                className = arr[arr.length - 1];
+                methodName = trace[j + 1].getMethodName();
+                lineNumber = trace[j + 1].getLineNumber();
                 break;
             }
         }
 
-        Log.d(String.format("<%s>.<%s>", className, methodName), text);
+        Log.d(String.format("%-16s %4d行目 %-16s===>", className, lineNumber, methodName), text);
 
     }
 
